ddcb83e8421cca7f2bc8549e6cf2b9e4a0037bec
Pouya <pouyajoon@gmail.com>
2014-08-12 00:32:03 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surfyphome::Application.config.secret_key_base = 'da6d4a6d9c0e4a91ec99a129a5a981e5f86ed69fbdc1af7599738db47b7d6fe49d8ed8acb424bad08b6bf250c0a8f863443472e8903b2eeb8d9900dbc7920a96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Surfyphome::Application.config.session_store :cookie_store, key: '_surfyphome_session'
