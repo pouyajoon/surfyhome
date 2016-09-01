fccecedb769ab291c2de7d51ebf4b7537ea3d469
Pouya Mohtacham <pouyajoon@gmail.com>
2016-09-01 18:48:55 +0200

    few updates

--- shared_memory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# simple shmsetup script
-# page_size=`getconf PAGE_SIZE`
-# phys_pages=`getconf _PHYS_PAGES`
-# shmall=`expr $phys_pages / 2`
-# shmmax=`expr $shmall \* $page_size`
-echo kernel.shmmax = 1055092736
-echo kernel.shmall = 257591
