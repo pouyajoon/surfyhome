eac2ffc4c9fd4e02e607a5c7d079453412f8b007
pouya mohtacham <pouyajoon@gmail.com>
2017-10-30 12:21:19 +0100

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Surfyphome::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.force_ssl = true
   
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -43,7 +42,7 @@ Surfyphome::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
